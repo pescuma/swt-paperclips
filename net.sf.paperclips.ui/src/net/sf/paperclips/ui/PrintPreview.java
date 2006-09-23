add7150c768a7b2251cccc1905e5b24e48dac0c6
qualidafial <qualidafial@localhost>
2006-09-23 06:51:39 +0000

    First working prototype of PrintPreview control.  Needs computeSize(int, int, boolean) method in order to calculate optimum size based on fit policy or scaling.

--- net.sf.paperclips.ui/src/net/sf/paperclips/ui/PrintPreview.java
+++ net.sf.paperclips.ui/src/net/sf/paperclips/ui/PrintPreview.java
@@ -4,9 +4,14 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.PaintEvent;
 import org.eclipse.swt.events.PaintListener;
 import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.Device;
 import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.graphics.RGB;
+import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.graphics.Transform;
+import org.eclipse.swt.printing.Printer;
 import org.eclipse.swt.printing.PrinterData;
 import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Composite;
@@ -14,7 +19,7 @@ import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Listener;
 
 import net.sf.paperclips.PaperClips;
-import net.sf.paperclips.Print;
+import net.sf.paperclips.PrintJob;
 import net.sf.paperclips.PrintPiece;
 
 /**
@@ -28,7 +33,7 @@ public class PrintPreview extends Canvas {
    * @param style the control style.
    */
   public PrintPreview(Composite parent, int style) {
-    super(parent, style);
+    super(parent, style | SWT.DOUBLE_BUFFERED);
 
     addPaintListener(new PaintListener() {
       public void paintControl(PaintEvent e) {
@@ -38,6 +43,8 @@ public class PrintPreview extends Canvas {
 
     addListener(SWT.Resize, new Listener() {
       public void handleEvent(Event event) {
+        if (fitVertical || fitHorizontal)
+          paperDisplayBounds = null;
         redraw();
       }
     });
@@ -49,66 +56,34 @@ public class PrintPreview extends Canvas {
     });
   }
 
-  private Print        print       = null;
-  private int          scaleMode   = SWT.HORIZONTAL | SWT.VERTICAL;
+  PrintJob    printJob      = null;
+  PrinterData printerData   = new PrinterData();
+  int         pageIndex     = 0;
+  boolean     fitHorizontal = true;
+  boolean     fitVertical   = true;
+  float       scale         = 1.0f;
 
-  private double       scale       = 1;
-  private PrintPiece[] pages       = null;
-  private PrinterData  printerData = null;
-  private int          orientation = PaperClips.PORTRAIT;
-  private int          pageIndex   = -1;
+  Printer   printer            = null;
+  Rectangle paperPrinterBounds = null; // The bounds of the paper on the printer device.
+
+  PrintPiece[] pages              = null;
+  Rectangle    paperDisplayBounds = null; // Where the paper is drawn within this control.
 
   /**
-   * Returns the scaling mode.
-   * @return the scaling mode.
+   * Returns the print job.
+   * @return the print job.
    */
-  public int getScaleMode() {
-    return scaleMode;
+  public PrintJob getPrintJob() {
+    return printJob;
   }
 
   /**
-   * Sets the scaling mode.
-   * @param mode a bitwise or of the directions which will be scaled to fit
+   * Sets the print job to preview.
+   * @param printJob the print job to preview.
    */
-  public void setScaleMode(int mode) {
-    this.scaleMode = mode & (SWT.HORIZONTAL | SWT.VERTICAL);
-  }
-
-  /**
-   * Returns the view scale.
-   * @return the view scale.
-   */
-  public double getScale() {
-    return scale;
-  }
-
-  /**
-   * Sets the view scale.
-   * @param scale the view scale.  A scale of 1.0 causes the document to appear at full size on the
-   *        computer screen.
-   */
-  public void setScale(double scale) {
-    if (scale > 0)
-      this.scale = scale;
-    else
-      throw new IllegalArgumentException("Scale must be > 0");
-  }
-
-  /**
-   * Returns the print being previewed.
-   * @return the print being previewed.
-   */
-  public Print getPrint() {
-    return print;
-  }
-
-  /**
-   * Sets the print to be previewed.
-   * @param print the print being previewed.
-   */
-  public void setPrint(Print print) {
-    this.print = print;
-    this.pageIndex = -1;
+  public void setPrintJob(PrintJob printJob) {
+    this.printJob = printJob;
+    this.pageIndex = 0;
     disposePages();
     redraw();
   }
@@ -128,56 +103,279 @@ public class PrintPreview extends Canvas {
   public void setPrinterData(PrinterData printerData) {
     this.printerData = printerData;
     this.pageIndex = -1;
-    disposePages();
+    disposePrinter(); // disposes pages
     redraw();
   }
 
   /**
-   * Returns the page orientation.
-   * @return the page orientation.
+   * Returns the page index.
+   * @return the page index.
    */
-  public int getOrientation() {
-    return orientation;
+  public int getPageIndex() {
+    return pageIndex;
   }
 
   /**
-   * Sets the page orientation.
-   * @param orientation the page orientation.  Must be one of {@link PaperClips#PORTRAIT} or
-   *        {@link PaperClips#LANDSCAPE}.
+   * Sets the page index.
+   * @param pageIndex the new page index.
    */
-  public void setOrientation(int orientation) {
-    this.orientation = orientation;
+  public void setPageIndex(int pageIndex) {
+    this.pageIndex = pageIndex;
+    redraw();
   }
 
-  public Point computeSize(int wHint, int hHint) {
-    return super.computeSize(wHint, hHint);
+  /**
+   * Returns the number of pages.  This method may return 0 if the selected page has not yet been
+   * displayed.
+   * @return the number of pages.
+   */
+  public int getPageCount() {
+    return pages == null ? 0 : pages.length;
+  }
+
+  /**
+   * Returns whether the page scales to fit the document horizontally.
+   * @return whether the page scales to fit the document horizontally.
+   */
+  public boolean isFitHorizontal() {
+    return fitHorizontal;
+  }
+
+  /**
+   * Sets whether the page scales to fit the document horizontally.
+   * @param fitHorizontal whether the page scales to fit the document horizontally.
+   */
+  public void setFitHorizontal(boolean fitHorizontal) {
+    if (this.fitHorizontal != fitHorizontal) {
+      this.fitHorizontal = fitHorizontal;
+      paperDisplayBounds = null;
+      redraw();
+    }
+  }
+
+  /**
+   * Returns whether the page scales to fit the document vertically.
+   * @return whether the page scales to fit the document vertically.
+   */
+  public boolean isFitVertical() {
+    return fitVertical;
+  }
+
+  /**
+   * Sets whether the page scales to fit the document vertically.
+   * @param fitVertical whether the page scales to fit the document vertically.
+   */
+  public void setFitVertical(boolean fitVertical) {
+    if (this.fitVertical != fitVertical) {
+      this.fitVertical = fitVertical;
+      paperDisplayBounds = null;
+      redraw();
+    }
+  }
+
+  /**
+   * Returns the view scale.  The document displays at this scale when !(isFitHorizontal() ||
+   * isFitVertical()). 
+   * @return the view scale.  
+   */
+  public float getScale() {
+    return scale;
+  }
+
+  /**
+   * Sets the view scale.
+   * @param scale the view scale.  A scale of 1.0 causes the document to appear at full size on the
+   *        computer screen.
+   */
+  public void setScale(float scale) {
+    if (scale > 0) {
+      this.scale = scale;
+      redraw();
+    }
+    else
+      throw new IllegalArgumentException("Scale must be > 0");
   }
 
   void paint(PaintEvent event) {
-    Image image = null;
-    GC gc = null;
+    Image printerImage = null;
+    GC printerGC = null;
+    Transform printerTransform = null;
+
+    Image displayImage = null;
+
     try {
-      image = new Image(event.display, event.width, event.height);
-
-      gc = new GC(image);
-  
-      Color oldBackground = gc.getBackground();
-      gc.setBackground(event.display.getSystemColor(SWT.COLOR_WIDGET_NORMAL_SHADOW));
-      gc.fillRectangle(event.x, event.y, event.width, event.height);
-      gc.setBackground(oldBackground);
-
-      if (print != null && printerData != null) {
-        
+      if (printJob == null || printerData == null) {
+        drawBackground(event, event.display, event.gc);
+        return;
       }
+
+      if (printer == null) {
+        printer = new Printer(printerData);
+        paperPrinterBounds = PaperClips.getPaperBounds(printer);
+      }
+
+      if (pages == null)
+        pages = PaperClips.getPages(printJob, printer);
+
+      if (paperDisplayBounds == null)
+        paperDisplayBounds = getPaperDisplayBounds();
+
+      if (pageIndex < 0 || pageIndex >= pages.length) {
+        drawBackground(event, event.display, event.gc);
+        return;
+      }
+
+      printerImage = new Image(printer, event.width, event.height);
+      printerGC = new GC(printerImage);
+
+      printerTransform = new Transform(printer);
+      printerGC.getTransform(printerTransform);
+      printerTransform.translate(-event.x, -event.y);
+      printerGC.setTransform(printerTransform);
+
+      drawBackground(event, printer, printerGC);
+      drawPaper(printer, printerGC);
+
+      printerTransform.translate(paperDisplayBounds.x, paperDisplayBounds.y);
+      float absoluteScale = getAbsoluteScale();
+      Point displayDPI = event.display.getDPI();
+      Point printerDPI = printer.getDPI();
+      float scaleX = absoluteScale * displayDPI.x / printerDPI.x;
+      float scaleY = absoluteScale * displayDPI.y / printerDPI.y;
+      printerTransform.scale(scaleX, scaleY);
+      printerGC.setTransform(printerTransform);
   
-      event.gc.drawImage(image, event.x, event.y);
+      pages[pageIndex].paint(printerGC, -paperPrinterBounds.x, -paperPrinterBounds.y);
+  
+      displayImage = new Image(event.display, printerImage.getImageData());
+      event.gc.drawImage(displayImage, event.x, event.y);
     } finally {
-      if (image != null)
-        image.dispose();
-      if (gc != null)
-        gc.dispose();
+      if (printerImage != null)
+        printerImage.dispose();
+      if (displayImage != null)
+        displayImage.dispose();
+      if (printerGC != null)
+        printerGC.dispose();
+      if (printerTransform != null)
+        printerTransform.dispose();
     }
-    image.dispose();
+  }
+
+  private static final int PAPER_MARGIN = 10;
+  private static final int PAPER_BORDER_WIDTH = 1;
+  private static final int PAPER_SHADOW_WIDTH = 3;
+
+  private final int BOILERPLATE_SIZE =
+    2*PAPER_MARGIN + 2*PAPER_BORDER_WIDTH + PAPER_SHADOW_WIDTH;
+
+  RGB background;
+
+  private void drawBackground(PaintEvent event, Device device, GC gc) {
+    Color oldBackground = gc.getBackground();
+
+    if (background == null)
+      background = event.display.getSystemColor(SWT.COLOR_WIDGET_NORMAL_SHADOW).getRGB();
+
+    Color bg = new Color(device, background);
+
+    gc.setBackground(bg);
+    gc.fillRectangle(event.x, event.y, event.width, event.height);
+    gc.setBackground(oldBackground);
+    bg.dispose();
+  }
+
+  private void drawPaper(Device device, GC gc) {
+    if (paperDisplayBounds == null) return;
+
+    Color white = device.getSystemColor(SWT.COLOR_WHITE);
+    Color black = device.getSystemColor(SWT.COLOR_BLACK);
+
+    // Drop shadow
+    gc.setBackground(black);
+    gc.fillRectangle(paperDisplayBounds.x+PAPER_SHADOW_WIDTH,
+                     paperDisplayBounds.y+PAPER_SHADOW_WIDTH,
+                     paperDisplayBounds.width+PAPER_BORDER_WIDTH,
+                     paperDisplayBounds.height+PAPER_BORDER_WIDTH);
+
+    // White page
+    gc.setBackground(white);
+    gc.fillRectangle(paperDisplayBounds);
+
+    // Page border
+    gc.setForeground(black);
+    for (int i = 1; i <= PAPER_BORDER_WIDTH; i++)
+      gc.drawRectangle(paperDisplayBounds.x-i,
+                       paperDisplayBounds.y-i,
+                       paperDisplayBounds.width+2*i-1,
+                       paperDisplayBounds.height+2*i-1);
+
+  }
+
+  /**
+   * Calculates the scale that the page should be displayed at on-screen.  This value is an 
+   * absolute scale based on physical measurements, and is independent of the relative DPI of the
+   * display and printer devices.  This means that the GC transform must be scaled by the result,
+   * as well as the display DPI to printer DPI ratio.
+   * <p>
+   * It is an error to call this method if the Printer field is null.
+   * @param display the display device.
+   * @param printer the printer device.
+   * @return the absolute scale that the page should be displayed, based on properties. 
+   */
+  private float getAbsoluteScale() {
+    if (fitHorizontal || fitVertical) {
+      Point displayDPI = getDisplay().getDPI();
+      Point printerDPI = printer.getDPI();
+      Point screenSize = getSize();
+      Rectangle paperSize = PaperClips.getPaperBounds(printer);
+      screenSize.x -= BOILERPLATE_SIZE;
+      screenSize.y -= BOILERPLATE_SIZE;
+
+      if (fitHorizontal) {
+        float screenWidth = (float) screenSize.x / (float) displayDPI.x; // inches
+        float paperWidth = (float) paperSize.width / (float) printerDPI.x; // inches
+        float scaleX = screenWidth / paperWidth;
+        if (fitVertical) {
+          float screenHeight = (float) screenSize.y / (float) displayDPI.y; // inches
+          float paperHeight = (float) paperSize.height / (float) printerDPI.y; // inches
+          float scaleY = screenHeight / paperHeight;
+          return Math.min(scaleX, scaleY);
+        }
+        return scaleX;
+      }
+      // fitVertical == true
+      float screenHeight = (float) screenSize.y / (float) displayDPI.y; // inches
+      float paperHeight = (float) paperSize.height / (float) printerDPI.y; // inches
+      float scaleY = screenHeight / paperHeight;
+      return scaleY;
+    }
+    return scale;
+  }
+
+  /**
+   * Returns the bounding rectangle where the paper is drawn on the control.  The paper border and
+   * drop shadow are drawn immediately outside this rectangle.
+   * @return the bounding rectangle where the paper is drawn on the control.
+   */
+  private Rectangle getPaperDisplayBounds() {
+    Point displayDPI = getDisplay().getDPI();
+    Point printerDPI = printer.getDPI();
+    float absoluteScale = getAbsoluteScale();
+    float scaleX = absoluteScale * displayDPI.x / printerDPI.x;
+    float scaleY = absoluteScale * displayDPI.y / printerDPI.y;
+
+    int x = PAPER_MARGIN + PAPER_BORDER_WIDTH;
+    int y = PAPER_MARGIN + PAPER_BORDER_WIDTH;
+    int width  = (int) Math.ceil(scaleX * paperPrinterBounds.width);
+    int height = (int) Math.ceil(scaleY * paperPrinterBounds.height);
+
+    // Center the paper horizontally if needed 
+    Point size = getSize();
+    size.x -= (width  + BOILERPLATE_SIZE);
+    if (size.x > 0)
+      x += size.x/2;
+
+    return new Rectangle(x, y, width, height);
   }
 
   private void disposePages() {
@@ -185,10 +383,23 @@ public class PrintPreview extends Canvas {
       for (int i = 0; i < pages.length; i++)
         pages[i].dispose();
       pages = null;
+      paperDisplayBounds = null;
+    }
+  }
+
+  private void disposePrinter() {
+    disposePages();
+    if (printer != null) {
+      printer.dispose();
+      printer = null;
+      paperPrinterBounds = null;
     }
   }
 
   void disposeResources() {
     disposePages();
+    disposePrinter();
   }
+
+  // TODO Implement computeSize(int, int, boolean)
 }
