b5625cc3c81c0a80c1948acb0b1477f40aafca18
qualidafial <qualidafial@localhost>
2007-04-19 16:27:34 +0000

    Adding cancelJob() and endJob() to disposePrinter() method in PrintPreview.  Addresses some incorrect usage of GTK printer resources by SWT.
    Moved RotateClockwisePrintPiece into its own source file.

--- net.sf.paperclips.ui/src/net/sf/paperclips/ui/PrintPreview.java
+++ net.sf.paperclips.ui/src/net/sf/paperclips/ui/PrintPreview.java
@@ -440,6 +440,8 @@ public class PrintPreview extends Canvas {
   private void disposePrinter() {
     disposePages();
     if (printer != null) {
+    	printer.cancelJob();
+    	printer.endJob();
       printer.dispose();
       printer = null;
       paperSize = null;
@@ -490,53 +492,5 @@ public class PrintPreview extends Canvas {
     size.y += Math.round( scale * paperSize.y * displayDPI.y / printerDPI.y );
 
     return size;
-  }
-}
-
-class RotateClockwisePrintPiece implements PrintPiece {
-  private final Printer printer;
-  private final PrintPiece target;
-  private final Point size;
-
-  RotateClockwisePrintPiece(Printer printer, PrintPiece target) {
-    if (printer == null || target == null)
-      throw new NullPointerException();
-    this.printer = printer;
-    this.target = target;
-    Point targetSize = target.getSize();
-    this.size = new Point(targetSize.y, targetSize.x);
-  }
-
-  public void dispose() {
-    target.dispose();
-  }
-
-  public Point getSize() {
-    return new Point(size.x, size.y);
-  }
-
-  public void paint(GC gc, int x, int y) {
-    Transform oldTransform = null;
-    Transform newTransform = null;
-    try {
-      oldTransform = new Transform(printer);
-      gc.getTransform(oldTransform);
-
-      newTransform = new Transform(printer);
-      gc.getTransform(newTransform);
-      newTransform.translate(x, y);
-      newTransform.translate(size.x, 0);
-      newTransform.rotate(90);
-      gc.setTransform(newTransform);
-
-      target.paint(gc, 0, 0);
-
-      gc.setTransform(oldTransform);
-    } finally {
-      if (oldTransform != null)
-        oldTransform.dispose();
-      if (newTransform != null)
-        newTransform.dispose();
-    }
   }
 }
