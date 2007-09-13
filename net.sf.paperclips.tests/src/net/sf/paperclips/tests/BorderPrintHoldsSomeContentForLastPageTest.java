ce5ccaccb797338fd6a7bd1334cbba0fc6e25409
qualidafial <qualidafial@localhost>
2007-09-13 23:32:50 +0000

    Adding copyright and authorship comments

--- net.sf.paperclips.tests/src/net/sf/paperclips/tests/BorderPrintHoldsSomeContentForLastPageTest.java
+++ net.sf.paperclips.tests/src/net/sf/paperclips/tests/BorderPrintHoldsSomeContentForLastPageTest.java
@@ -1,5 +1,12 @@
-package net.sf.paperclips.tests;
+/************************************************************************************************************
+ * Copyright (c) 2007 Woodcraft Mill & Cabinet Corporation. All rights reserved. This program and the
+ * accompanying materials are made available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: Woodcraft Mill & Cabinet Corporation - initial API and implementation
+ ***********************************************************************************************************/
 
+package net.sf.paperclips.tests;
 
 import net.sf.paperclips.BorderPrint;
 import net.sf.paperclips.ColumnPrint;
@@ -46,7 +53,6 @@ public class BorderPrintHoldsSomeContentForLastPageTest extends FactoryPrint {
     Display.getDefault();
 
     PaperClips.print( new PrintJob( "BreakPrintExample.java",
-                                    new BorderPrintHoldsSomeContentForLastPageTest() ),
-                      new PrinterData() );
+                                    new BorderPrintHoldsSomeContentForLastPageTest() ), new PrinterData() );
   }
 }
--- net.sf.paperclips.ui/src/net/sf/paperclips/ui/PrintPreview.java
+++ net.sf.paperclips.ui/src/net/sf/paperclips/ui/PrintPreview.java
@@ -1,3 +1,10 @@
+/************************************************************************************************************
+ * Copyright (c) 2006 Woodcraft Mill & Cabinet Corporation. All rights reserved. This program and the
+ * accompanying materials are made available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: Woodcraft Mill & Cabinet Corporation - initial API and implementation
+ ***********************************************************************************************************/
 package net.sf.paperclips.ui;
 
 import org.eclipse.swt.SWT;
@@ -389,10 +396,9 @@ public class PrintPreview extends Canvas {
     Printer printer = getPrinter();
     if ( paperSize == null && printer != null && printJob != null ) {
       Rectangle paperBounds = PaperClips.getPaperBounds( printer );
-      this.paperSize =
-          orientationRequiresRotate()
-              ? new Point( paperBounds.height, paperBounds.width )
-              : new Point( paperBounds.width, paperBounds.height );
+      this.paperSize = orientationRequiresRotate()
+          ? new Point( paperBounds.height, paperBounds.width )
+          : new Point( paperBounds.width, paperBounds.height );
     }
     return paperSize;
   }
