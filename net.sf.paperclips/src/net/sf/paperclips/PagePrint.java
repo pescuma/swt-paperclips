938380369c00620f30c254a88e7dbee3e686b572
qualidafial <qualidafial@localhost>
2007-10-05 23:41:27 +0000

    Refactor PagePrint to improve readability and clarity.

--- net.sf.paperclips/src/net/sf/paperclips/PagePrint.java
+++ net.sf.paperclips/src/net/sf/paperclips/PagePrint.java
@@ -8,12 +8,12 @@
 package net.sf.paperclips;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
-import org.eclipse.swt.graphics.Device;
-import org.eclipse.swt.graphics.GC;
-import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.*;
+
+import net.sf.paperclips.internal.*;
 
 /**
  * A decorator Print which displays page headers and footers around a document body, with page numbering
@@ -21,97 +21,104 @@ import org.eclipse.swt.graphics.Point;
  * <p>
  * PagePrint is horizontally and vertically greedy. Greedy prints take up all the available space on the
  * page.
+ * <p>
+ * <b>Note:</b> Avoid wrapping PagePrint in prints with space-optimizing semantics (e.g. ColumnPrint
+ * equalizes columns on the last page), as this may cause the total page count to be incorrect on some pages.
+ * At this time there is no known fix. If wrapping a PagePrint is unavoidable, consider using a custom
+ * PageNumberFormat which does not display the total page count.
+ * 
  * @author Matthew Hall
  */
 public class PagePrint implements Print {
-  Print          body;
+  private static final int DEFAULT_GAP = 1;
 
-  PageDecoration header;
-  PageDecoration footer;
+  PageDecoration           header;
+  int                      headerGap   = DEFAULT_GAP; // in points
+  Print                    body;
+  int                      footerGap   = DEFAULT_GAP; // in points
+  PageDecoration           footer;
 
-  int            headerGap = 1; // in points
-  int            footerGap = 1; // in points
+  /**
+   * Constructs a PagePrint with the given header and body.
+   * @param header a PageDecoration for creating the header. May be null.
+   * @param headerGap the gap between the header and body, in points.
+   * @param body the Print being decorated.
+   */
+  public PagePrint( PageDecoration header, int headerGap, Print body ) {
+    this( header, headerGap, body, DEFAULT_GAP, null );
+  }
+
+  /**
+   * Constructs a PagePrint with the given header and body.
+   * @param body the Print being decorated.
+   * @param header a PageDecoration for creating the header. May be null.
+   */
+  public PagePrint( PageDecoration header, Print body ) {
+    this( header, DEFAULT_GAP, body );
+  }
+
+  /**
+   * Constructs a PagePrint with the given body.
+   * @param body the Print being decorated.
+   */
+  public PagePrint( Print body ) {
+    this( null, body, null );
+  }
+
+  /**
+   * Constructs a PagePrint with the given body and footer.
+   * @param body the Print being decorated.
+   * @param footer a PageDecoration for creating the footer. may be null.
+   */
+  public PagePrint( Print body, PageDecoration footer ) {
+    this( body, DEFAULT_GAP, footer );
+  }
+
+  /**
+   * Constructs a PagePrint with the given body, header and footer.
+   * @param body the Print being decorated.
+   * @param footerGap the gap between the body and footer, in points.
+   * @param footer a PageDecoration for creating the footer. May be null.
+   */
+  public PagePrint( Print body, int footerGap, PageDecoration footer ) {
+    this( null, DEFAULT_GAP, body, footerGap, footer );
+  }
 
   /**
    * Constructs a PagePrint with the given body, header and footer.
    * @param body the Print being decorated.
    * @param header a PageDecoration for creating the header. May be null.
    * @param footer a PageDecoration for creating the footer. may be null.
+   * @deprecated PagePrint(PageDecoration, Print, PageDecoration) instead.
    */
   public PagePrint( Print body, PageDecoration header, PageDecoration footer ) {
-    if ( body == null )
-      throw new NullPointerException();
-    this.body = body;
-    this.header = header;
-    this.footer = footer;
+    this( header, body, footer );
   }
 
   /**
-   * Constructs a PagePrint with the given body, and no header or footer.
+   * Constructs a PagePrint with the given body, header and footer.
+   * @param header a PageDecoration for creating the header. May be null.
    * @param body the Print being decorated.
+   * @param footer a PageDecoration for creating the footer. may be null.
    */
-  public PagePrint( Print body ) {
-    if ( body == null )
-      throw new NullPointerException();
-    this.body = body;
-    this.header = null;
-    this.footer = null;
-  }
-
-  private int checkGap( int gap ) {
-    if ( gap < 0 )
-      throw new IllegalArgumentException( "Gap must be >= 0 (value is " + gap + ")" );
-    return gap;
+  public PagePrint( PageDecoration header, Print body, PageDecoration footer ) {
+    this( header, DEFAULT_GAP, body, DEFAULT_GAP, footer );
   }
 
   /**
-   * Sets the page body to the argument.
-   * @param body the new page body.
+   * Constructs a PagePrint with the given body, header and footer.
+   * @param header a PageDecoration for creating the header. May be null.
+   * @param headerGap the gap between the header and body, in points.
+   * @param body the Print being decorated.
+   * @param footerGap the gap between the body and footer, in points.
+   * @param footer a PageDecoration for creating the footer. May be null.
    */
-  public void setBody( Print body ) {
-    if ( body == null )
-      throw new NullPointerException();
-    this.body = body;
-  }
-
-  /**
-   * Sets the page header to the argument.
-   * @param header a PageDecoration which creates the header. May be null.
-   */
-  public void setHeader( PageDecoration header ) {
-    this.header = header;
-  }
-
-  /**
-   * Sets the gap between the header and body to the argument, expressed in points.
-   * @param points the new gap between the header and body (if there is a header).
-   */
-  public void setHeaderGap( int points ) {
-    this.headerGap = checkGap( points );
-  }
-
-  /**
-   * Sets the page footer to the argument.
-   * @param footer a PageDecoration which creates the footer. May be null.
-   */
-  public void setFooter( PageDecoration footer ) {
-    this.footer = footer;
-  }
-
-  /**
-   * Sets the gap between the body and footer to the argument, expressed in points.
-   * @param points the new gap between the body and footer (if there is a footer).
-   */
-  public void setFooterGap( int points ) {
-    this.footerGap = checkGap( points );
-  }
-
-  /**
-   * Returns the page body.
-   * @return the page body.
-   */
-  public Print getBody() {
-    return body;
+  public PagePrint( PageDecoration header, int headerGap, Print body, int footerGap, PageDecoration footer ) {
+    setHeader( header );
+    setHeaderGap( headerGap );
+    setBody( body );
+    setFooterGap( footerGap );
+    setFooter( footer );
   }
 
   /**
@@ -123,11 +130,44 @@ public class PagePrint implements Print {
   }
 
   /**
+   * Sets the page header to the argument.
+   * @param header a PageDecoration which creates the header. May be null.
+   */
+  public void setHeader( PageDecoration header ) {
+    this.header = header;
+  }
+
+  /**
    * Returns the gap between the header and body, expressed in points.
    * @return the gap between the header and body, expressed in points.
    */
   public int getHeaderGap() {
     return headerGap;
+  }
+
+  /**
+   * Sets the gap between the header and body to the argument, expressed in points.
+   * @param points the new gap between the header and body, expressed in points. 72 points = 1".
+   */
+  public void setHeaderGap( int points ) {
+    this.headerGap = checkGap( points );
+  }
+
+  /**
+   * Returns the page body.
+   * @return the page body.
+   */
+  public Print getBody() {
+    return body;
+  }
+
+  /**
+   * Sets the page body to the argument.
+   * @param body the new page body.
+   */
+  public void setBody( Print body ) {
+    NullUtil.notNull( body );
+    this.body = body;
   }
 
   /**
@@ -139,6 +179,14 @@ public class PagePrint implements Print {
   }
 
   /**
+   * Sets the page footer to the argument.
+   * @param footer a PageDecoration which creates the footer. May be null.
+   */
+  public void setFooter( PageDecoration footer ) {
+    this.footer = footer;
+  }
+
+  /**
    * Returns the gap between the body and footer, expressed in points.
    * @return the gap between the body and footer, expressed in points.
    */
@@ -146,10 +194,21 @@ public class PagePrint implements Print {
     return footerGap;
   }
 
+  /**
+   * Sets the gap between the body and footer to the argument, expressed in points.
+   * @param points the new gap between the body and footer (if there is a footer).
+   */
+  public void setFooterGap( int points ) {
+    this.footerGap = checkGap( points );
+  }
+
+  private static int checkGap( int gap ) {
+    if ( gap < 0 )
+      PaperClips.error( SWT.ERROR_INVALID_ARGUMENT, "Gap must be >= 0 (value is " + gap + ")" );
+    return gap;
+  }
+
   public PrintIterator iterator( Device device, GC gc ) {
-    PageDecoration header = this.header;
-    PageDecoration footer = this.footer;
-    // If there is no header or footer, just fall through to the body iterator.
     if ( header == null && footer == null )
       return body.iterator( device, gc );
 
@@ -157,54 +216,52 @@ public class PagePrint implements Print {
   }
 }
 
-class PageNumberer {
-  int pageCount = 0;
-
-  synchronized PageNumber next() {
-    return new InnerPageNumber();
-  }
-
-  class InnerPageNumber implements PageNumber {
-    final int pageNumber = pageCount++; // POST-increment
-
-    public int getPageCount() {
-      return pageCount;
-    }
-
-    public int getPageNumber() {
-      return pageNumber;
-    }
-  }
-
-  PageNumberer copy() {
-    PageNumberer result = new PageNumberer();
-    result.pageCount = this.pageCount;
-    return result;
-  }
-}
-
 class PageIterator implements PrintIterator {
+  class PageNumberer {
+    int pageCount = 0;
+
+    synchronized PageNumber next() {
+      return new InnerPageNumber();
+    }
+
+    class InnerPageNumber implements PageNumber {
+      final int pageNumber = pageCount++; // POST-increment
+
+      public int getPageCount() {
+        return pageCount;
+      }
+
+      public int getPageNumber() {
+        return pageNumber;
+      }
+    }
+
+    PageNumberer copy() {
+      PageNumberer result = new PageNumberer();
+      result.pageCount = this.pageCount;
+      return result;
+    }
+  }
+
   final Device         device;
   final GC             gc;
-  final Point          dpi;
 
   final PageDecoration header;
   final int            headerGap;    // pixels
-
   final PrintIterator  body;
-
-  final PageDecoration footer;
   final int            footerGap;    // pixels
+  final PageDecoration footer;
 
   final PageNumberer   numberer;
 
-  Point                minimumSize;
-  Point                preferredSize;
+  final Point          minimumSize;
+  final Point          preferredSize;
 
   PageIterator( PagePrint print, Device device, GC gc ) {
     this.device = device;
     this.gc = gc;
-    this.dpi = device.getDPI();
+
+    Point dpi = device.getDPI();
 
     body = print.body.iterator( device, gc );
     header = print.header;
@@ -213,12 +270,14 @@ class PageIterator implements PrintIterator {
     footerGap = footer == null ? 0 : print.footerGap * dpi.y / 72;
 
     this.numberer = new PageNumberer();
+
+    this.minimumSize = computeSize( PrintSizeStrategy.MINIMUM );
+    this.preferredSize = computeSize( PrintSizeStrategy.PREFERRED );
   }
 
   PageIterator( PageIterator that ) {
     this.device = that.device;
     this.gc = that.gc;
-    this.dpi = that.dpi;
 
     this.body = that.body.copy();
     this.header = that.header;
@@ -226,19 +285,15 @@ class PageIterator implements PrintIterator {
     this.footer = that.footer;
     this.footerGap = that.footerGap;
 
+    // FIXME: Wrapping PagePrint in a class with space-optimizing semantics (ColumnPrint) can fork the total
+    // page count. i.e. if the copied PageIterator is chosen as the optimal layout, then previous pages will
+    // have a page number spawned from a different page numberer. Thus the total page count for those
+    // previous pages will no longer be incremented with each new page.
     this.numberer = that.numberer.copy();
     this.pageNumber = that.pageNumber;
 
     this.minimumSize = that.minimumSize;
     this.preferredSize = that.preferredSize;
-  }
-
-  private void computeSizes() {
-    if ( minimumSize == null )
-      minimumSize = computeSize( PrintSizeStrategy.MINIMUM );
-    if ( preferredSize == null )
-      preferredSize = computeSize( PrintSizeStrategy.PREFERRED );
-
   }
 
   private Point computeSize( PrintSizeStrategy strategy ) {
@@ -288,84 +343,97 @@ class PageIterator implements PrintIterator {
   }
 
   public Point minimumSize() {
-    if ( minimumSize == null )
-      computeSizes();
-    return minimumSize;
+    return new Point( minimumSize.x, minimumSize.y );
   }
 
   public Point preferredSize() {
-    if ( preferredSize == null )
-      computeSizes();
-    return preferredSize;
+    return new Point( preferredSize.x, preferredSize.y );
   }
 
-  private PageNumber pageNumber = null;
-
-  public PrintPiece next( int width, int height ) {
-    if ( pageNumber == null )
-      pageNumber = numberer.next();
-
-    int y = 0;
-
-    List entries = new ArrayList();
+  public PrintPiece next( int width, final int height ) {
+    PageNumber pageNumber = getCurrentPageNumber();
 
     // HEADER
+    PrintPiece headerPiece = null;
+    int availableHeight = height;
     if ( header != null ) {
       Print headerPrint = header.createPrint( pageNumber );
       if ( headerPrint != null ) {
-        PrintPiece headerPiece = getDecorationPrintPiece( headerPrint, width, height );
+        headerPiece = getDecorationPrintPiece( headerPrint, width, availableHeight );
         if ( headerPiece == null )
           return null;
-
-        entries.add( new CompositeEntry( headerPiece, new Point( 0, 0 ) ) );
-
-        int headerSize = headerPiece.getSize().y + headerGap;
-        y += headerSize;
-        height -= headerSize;
+        availableHeight -= ( heightOf( headerPiece ) + headerGap );
       }
     }
 
     // FOOTER
+    PrintPiece footerPiece = null;
     if ( footer != null ) {
       Print footerPrint = footer.createPrint( pageNumber );
       if ( footerPrint != null ) {
-        PrintPiece footerPiece = getDecorationPrintPiece( footerPrint, width, height );
+        footerPiece = getDecorationPrintPiece( footerPrint, width, availableHeight );
         if ( footerPiece == null ) {
-          return nukeEntries( entries );
+          PrintPieceUtil.dispose( headerPiece );
+          return null;
         }
-
-        entries.add( new CompositeEntry( footerPiece, new Point( 0, y + height - footerPiece.getSize().y ) ) );
-
-        int footerSize = footerPiece.getSize().y + footerGap;
-
-        height -= footerSize;
+        availableHeight -= ( heightOf( footerPiece ) + footerGap );
       }
     }
 
     // BODY
-    PrintPiece bodyPiece = PaperClips.next( body, width, height );
+    PrintPiece bodyPiece = PaperClips.next( body, width, availableHeight );
     if ( bodyPiece == null ) {
-      return nukeEntries( entries );
+      PrintPieceUtil.dispose( headerPiece, footerPiece );
+      return null;
     }
 
-    entries.add( new CompositeEntry( bodyPiece, new Point( 0, y ) ) );
-
-    // Compile and return page.
-    PrintPiece result = new CompositePiece( entries );
-
-    // Iteration successful. Null the pageNumber field so the next iteration
-    // advances to the next page.
-    pageNumber = null;
-
+    PrintPiece result = createResult( height, headerPiece, bodyPiece, footerPiece );
+    advancePageNumber();
     return result;
   }
 
-  private PrintPiece nukeEntries( List entries ) {
-    for ( Iterator iter = entries.iterator(); iter.hasNext(); ) {
-      CompositeEntry entry = (CompositeEntry) iter.next();
-      entry.piece.dispose();
+  private int heightOf( PrintPiece piece ) {
+    return piece.getSize().y;
+  }
+
+  PageNumber pageNumber;
+
+  private void advancePageNumber() {
+    // Null the pageNumber field so the next iteration advances to the next page.
+    pageNumber = null;
+  }
+
+  private PageNumber getCurrentPageNumber() {
+    if ( pageNumber == null )
+      pageNumber = numberer.next();
+    return pageNumber;
+  }
+
+  private PrintPiece createResult( int height,
+                                   PrintPiece headerPiece,
+                                   PrintPiece bodyPiece,
+                                   PrintPiece footerPiece ) {
+    if ( headerPiece == null && footerPiece == null )
+      return bodyPiece;
+
+    List entries = new ArrayList();
+
+    if ( headerPiece != null )
+      entries.add( createEntry( headerPiece, 0 ) );
+
+    int y = headerPiece == null ? 0 : heightOf( headerPiece ) + headerGap;
+    entries.add( createEntry( bodyPiece, y ) );
+
+    if ( footerPiece != null ) {
+      y = height - heightOf( footerPiece );
+      entries.add( createEntry( footerPiece, y ) );
     }
-    return null;
+
+    return new CompositePiece( entries );
+  }
+
+  private CompositeEntry createEntry( PrintPiece piece, int y ) {
+    return new CompositeEntry( piece, new Point( 0, y ) );
   }
 
   private PrintPiece getDecorationPrintPiece( Print decoration, int width, int height ) {
